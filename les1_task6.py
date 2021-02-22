b906318516d86654fceeda8853699b8d9dab8960
Maxim <max1361@yandex.ru>
2021-02-22 15:15:50 +0300

    first commit2

---
+++ les1_task6.py
@@ -0,0 +1,6 @@
+# 6
+# Пользователь вводит номер буквы в алфавите. Определить, какая это буква
+
+character = int(input("Введите номер символа: \n >>> "))
+chr_result = chr(character)
+print(chr_result)
