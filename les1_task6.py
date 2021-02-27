cf9fb6fcef239cab6a50065011675044e4011d6c
Maxim <max1361@yandex.ru>
2021-02-27 15:00:16 +0300

    new2

--- les1_task6.py
+++
@@ -1,6 +0,0 @@
-# 6
-# Пользователь вводит номер буквы в алфавите. Определить, какая это буква
-
-character = int(input("Введите номер символа: \n >>> "))
-chr_result = chr(character)
-print(chr_result)
